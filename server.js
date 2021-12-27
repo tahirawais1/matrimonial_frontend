905e555160fa7366154ef67d58914710eaaf49d1
Your Name <you@example.com>
2021-12-27 23:43:47 +0500

    test

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 //Install express server
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/matrimonial'));
+app.use(express.static(__dirname + "/dist/matrimonial"));
 
-app.get('/*', function(req,res) {
-
-res.sendFile(path.join(__dirname+'/dist/matrimonial/index.html'));
+app.get("/*", function (req, res) {
+  res.sendFile("index.html", {root: "dist/matrimonial"})
 });
 
 // Start the app by listening on the default Heroku port
